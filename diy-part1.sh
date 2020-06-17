88a0d2179b91a947bb664153df30c398ab9466da
^_^ <iamlinli@qq.com>
2020-06-17 20:37:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,15 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add custom software packages 添加自定义软件包 
+# - 需要在diy-part2.sh脚本编写追加自定义内容至.config
+mkdir -p package/others
+# theme
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/others/luci-theme-atmateria
+# clash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/others/luci-app-clash
